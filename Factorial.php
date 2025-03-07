14bc5e5156b40cb64a7cb1c684eca5f1a7863bcc
gangadhar195 <gangadhars9545@gmail.com>
2025-03-07 10:22:15 +0530

    fact

---
+++ Factorial.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Factorial No </title>
+    <style>
+        table , td{
+            border: 1px solid black;
+        }
+        .btn{
+            align-items: center;
+            text-align: center;
+            padding: 4px;
+        }
+    </style>
+</head>
+<body>
+    <div class="form">
+        <form> 
+            <table>
+                <tr>
+                    <td>
+                        Enter No :
+                    </td>
+                    <td>
+                        <input type="text" name="n" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="btn" colspan="2">
+                        <input type="submit" name="submit" value="submit"/>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+</html>
