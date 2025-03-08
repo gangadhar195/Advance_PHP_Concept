2024192deffe2c0762303ab8191a0987e68c5746
gangadhar195 <gangadhars9545@gmail.com>
2025-03-09 00:12:01 +0530

    factorial and fibonacci program

--- fact_and_fibo.php
+++ fact_and_fibo.php
@@ -1,43 +1,36 @@
-<?php
-if(isset($_REQUEST['submit']))
-{
-    $n = $_REQUEST['n'];
-    $fact = 1;
-    for($i = 2;$i<=$n;$i++){
-        $fact *= $i; 
-    }
-    echo "Factorial of No ".$n." is : ".$fact ."<br>";
-    $n1=0;
-    $n2=1;
-    echo"Fibonacci Series No ".$n." : ";
-    for($i = 0;$i<$n;$i++){
-        echo" ".$n1;
-        $n3=$n1+$n2;
-        $n1=$n2;
-        $n2=$n3;
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Factorial and Fibonacci Program </title>
     <style>
-        table , td{
+        table,
+        td {
             border: 1px solid black;
         }
-        .btn{
+        table{
+            margin-bottom: 20px;
+        }
+
+        .btn {
             align-items: center;
             text-align: center;
             padding: 4px;
         }
+
+        .form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
     </style>
 </head>
+
 <body>
     <div class="form">
-        <form> 
+        <form>
             <table>
                 <tr>
                     <td>
@@ -49,11 +42,38 @@ if(isset($_REQUEST['submit']))
                 </tr>
                 <tr>
                     <td class="btn" colspan="2">
-                        <input type="submit" name="submit" value="submit"/>
+                        <input type="submit" name="submit" value="submit" />
                     </td>
                 </tr>
             </table>
+            <?php
+            if (isset($_REQUEST['submit'])) {
+                $n = $_REQUEST['n'];
+                $fact = 1;
+                for ($i = 2; $i <= $n; $i++) {
+                    $fact *= $i;
+                }
+                ?>
+                <strong>
+                    <?php
+                  echo "Factorial of No " . $n . " is : " . $fact . "<br>";
+                    
+                    ?>
+                </strong>
+                <?php
+                $n1 = 0;
+                $n2 = 1;
+                echo "Fibonacci Series No " . $n . " : ";
+                for ($i = 0; $i < $n; $i++) {
+                    echo " " . $n1;
+                    $n3 = $n1 + $n2;
+                    $n1 = $n2;
+                    $n2 = $n3;
+                }
+            }
+            ?>
         </form>
     </div>
 </body>
+
 </html>
