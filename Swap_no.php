ce666a83d7e78f244481289518f7e4ca9f57cefe
gangadhar195 <gangadhars9545@gmail.com>
2025-03-10 12:29:08 +0530

    Swape No

---
+++ Swap_no.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Factorial and Fibonacci Program </title>
+    <style>
+        table,
+        td {
+            border: 1px solid black;
+        }
+
+        table {
+            margin-bottom: 20px;
+        }
+
+        .btn {
+            align-items: center;
+            text-align: center;
+            padding: 4px;
+        }
+
+        .form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .output {
+            background-color: black;
+            color: yellow;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="form">
+        <form>
+            <table>
+                <tr>
+                    <td>
+                        Enter No 1 :
+                    </td>
+                    <td>
+                        <input type="text" name="n1" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Enter No 2 :
+                    </td>
+                    <td>
+                        <input type="text" name="n2" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="btn" colspan="2">
+                        <input type="submit" name="submit" value="submit" />
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+</body>
+
+</html>
